29aa75cc98a3dd26339a6b5614877c30ee9a2536
ExMeWTF <qwest4635@gmail.com>
2021-12-13 03:06:10 -0800

    setUp

--- babel.config.js
+++ babel.config.js
@@ -5,11 +5,9 @@ module.exports = api => {
         "@babel/preset-env",
         "@babel/preset-react",
         "@babel/preset-typescript",
-        "mobx",
     ]
 
     const plugins = [
-        "@babel/plugin-proposal-optional-chaining",
         process.env.NODE_ENV === 'development' && 'react-refresh/babel'
     ].filter(Boolean);
 
