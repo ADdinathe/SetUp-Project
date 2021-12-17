d135283a7c34fa4df4d61fd77547766d3e3cef9d
ExMeWTF <qwest4635@gmail.com>
2021-12-17 03:35:15 -0800

    [*]PhoneMask

--- src/components/PhoneMask/PhoneMask.tsx
+++ src/components/PhoneMask/PhoneMask.tsx
@@ -1,6 +1,5 @@
-    //TODO: стиль на скрол в списке; -hz kak ego delat
-    //  всякие оптимизации реактовские и причесать коду меня сегодня будет много вопросов по рефам, я не могу придумать как понять на какой именно инпут нажимают
-    // и еще есть проблема я не до конца в типах разобрался наверное, но я
+//TODO: стиль на скрол в списке; -hz kak ego delat
+//  всякие оптимизации реактовские и причесать код
 import React, {useRef, useState} from 'react';
 import './PhoneMask.css';
 import {COUNTRIES} from './config';
@@ -20,11 +19,13 @@ type PhoneMaskProps = {
     disabled: boolean;
 };
 
+
 const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
     const [countryCode, setcountryCode] = useState(COUNTRIES[0].label);
     const [values, setValues] = React.useState(Array(10).fill(''));
     const [style, setStyle] = useState<string>("input");
-    const inputEl = useRef(null);
+    const inputRef = useRef(null);
+    const revealRef = useRef<HTMLInputElement[]>([]);
 
     const handleSelectChange = React.useCallback(
         (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -35,9 +36,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
     );
 
     const phoneCheck = (phoneCode: string, phoneNum: string) => {
-        setStyle('loading');
         if (
-            !!COUNTRIES.find((item) => item.label === phoneCode) &&
             phoneNum.length == 10
         ) {
             setStyle('success');
@@ -45,7 +44,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
         }
         setStyle('error');
     };
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
         if (!(event.target instanceof HTMLElement)) {
             return;
         }
@@ -57,12 +56,14 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
         }
         if (event.key === 'ArrowLeft') {
 
-            if (undefined !== event.target.dataset.index) {
+            if (event.target.dataset.index !== undefined) {
 
-                const index = +event.target.dataset.index;
+                const index = Number(event.target.dataset.index);
                 if (index > 0) {
-                    inputRefs[index - 1].focus();
-                    inputRefs[index - 1].select();
+                    revealRef.current[index - 1].focus()
+                    revealRef.current[index - 1].select();
+                    // inputRefs[index - 1].focus();
+                    // inputRefs[index - 1].select();
                 }
             }
         }
@@ -72,8 +73,8 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
 
                 const index = +event.target.dataset.index;
                 if (index < values.length - 1) {
-                    inputRefs[index + 1].focus();
-                    inputRefs[index + 1].select();
+                    revealRef.current[index + 1].focus()
+                    revealRef.current[index + 1].select();
                 }
             }
         }
@@ -82,10 +83,10 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
             if (undefined !== event.target.dataset.index) {
 
                 const index = +event.target.dataset.index;
-                if (index > 0) {
+                if (index > 0 && values[index]=='') {
                     setValues(values.map((n, i) => (i === index ? '' : n)));
-                    inputRefs[index - 1].focus();
-                    inputRefs[index - 1].select();
+                    revealRef.current[index - 1].focus()
+                    // revealRef.current[index - 1].select();
                 }
             }
         }
@@ -98,14 +99,23 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
             const index = +event.target.dataset.index;
             const value = event.target.value;
 
-            setValues(values.map((n, i) => (i === index ? event.target.value.slice(0,1) : n)));
+            setValues(values.map((n, i) => (i === index ? event.target.value.slice(0, 1) : n)));
 
             if (index < values.length - 1 && value) {
-                inputRefs[index + 1].focus();
-                inputRefs[index + 1].select();
+                revealRef.current[index + 1].focus()
+                revealRef.current[index + 1].select();
             }
         }
     };
+
+    const myRefs = (el: any) => {
+        if (el && !revealRef.current.includes(el)) {
+            revealRef.current.push(el)
+        }
+        console.log(revealRef)
+    }
+
+
     return (
         <MainForm>
             <StyledLabel>Введите номер телефона</StyledLabel>
@@ -135,7 +145,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     max="10"
                     tabIndex={2}
 
-                    ref={(input) => ( input? inputRefs[0] = input : console.log("123"))}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -149,7 +159,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={1}
                     max="10"
                     tabIndex={3}
-                    ref={(input) => (inputRefs[1] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -164,7 +174,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={2}
                     max="10"
                     tabIndex={4}
-                    ref={(input) => (inputRefs[2] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -179,7 +189,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={3}
                     max="10"
                     tabIndex={5}
-                    ref={(input) => (inputRefs[3] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -193,7 +203,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={4}
                     max="10"
                     tabIndex={6}
-                    ref={(input) => (inputRefs[4] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -207,7 +217,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={5}
                     max="10"
                     tabIndex={7}
-                    ref={(input) => (inputRefs[5] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />-
@@ -221,7 +231,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={6}
                     max="10"
                     tabIndex={8}
-                    ref={(input) => (inputRefs[6] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -235,7 +245,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={7}
                     max="10"
                     tabIndex={9}
-                    ref={(input) => (inputRefs[7] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />-
@@ -249,7 +259,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={8}
                     max="10"
                     tabIndex={10}
-                    ref={(input) => (inputRefs[8] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -263,7 +273,7 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
                     data-index={9}
                     max="10"
                     tabIndex={11}
-                    ref={(input) => (inputRefs[9] = input as HTMLInputElement)}
+                    ref={myRefs}
                     onChange={onChange}
                     onKeyDown={handleKeyDown}
                 />
@@ -288,18 +298,22 @@ const PhoneMask: React.FC<PhoneMaskProps> = ({disabled}) => {
             </MainMask>
             {style === Meta.error && (<ErrorPic>
                     <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.666656 13H15.3333L7.99999 0.333313L0.666656 13ZM8.66666 11H7.33332V9.66665H8.66666V11ZM8.66666 8.33331H7.33332V5.66665H8.66666V8.33331Z" fill="#F03738"/>
+                        <path
+                            d="M0.666656 13H15.3333L7.99999 0.333313L0.666656 13ZM8.66666 11H7.33332V9.66665H8.66666V11ZM8.66666 8.33331H7.33332V5.66665H8.66666V8.33331Z"
+                            fill="#F03738"/>
                     </svg>
                     <StyledErrorBlock>Неправильный номер телефона</StyledErrorBlock>
-            </ErrorPic>
+                </ErrorPic>
 
             )}
             {style === Meta.success && (<ErrorPic>
-                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7.00001 0.333313C3.32001 0.333313 0.333344 3.31998 0.333344 6.99998C0.333344 10.68 3.32001 13.6666 7.00001 13.6666C10.68 13.6666 13.6667 10.68 13.6667 6.99998C13.6667 3.31998 10.68 0.333313 7.00001 0.333313ZM5.66668 10.3333L2.33334 6.99998L3.27334 6.05998L5.66668 8.44665L10.7267 3.38665L11.6667 4.33331L5.66668 10.3333Z" fill="#3CC13B"/>
-                </svg>
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M7.00001 0.333313C3.32001 0.333313 0.333344 3.31998 0.333344 6.99998C0.333344 10.68 3.32001 13.6666 7.00001 13.6666C10.68 13.6666 13.6667 10.68 13.6667 6.99998C13.6667 3.31998 10.68 0.333313 7.00001 0.333313ZM5.66668 10.3333L2.33334 6.99998L3.27334 6.05998L5.66668 8.44665L10.7267 3.38665L11.6667 4.33331L5.66668 10.3333Z"
+                            fill="#3CC13B"/>
+                    </svg>
                     <StyledSuccessBlock>Номер телефона введен верно</StyledSuccessBlock>
-            </ErrorPic>
+                </ErrorPic>
 
             )}
         </MainForm>
